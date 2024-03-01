916ded3b3263559c53dfe876b7af9f65fb8f5547
George Pipkin <gpp8p@Virginia.EDU>
2024-03-01 17:40:04 -0500

    start on spTable - a replacement for oruga tables

---
+++ src/components/spTable.vue
@@ -0,0 +1,210 @@
+<template>
+  <span>
+    <table class="styled-table">
+      <th v-for="(thisColumn, index) in this.styledColumns"
+        :key="index"
+        >
+        <span :style="thisColumn.columnStyle" v-if="thisColumn.visible">{{thisColumn.label}}</span>
+      </th>
+    </table>
+  </span>
+</template>
+
+<script>
+import utils from '../components/utils.vue';
+
+export default {
+  name: "menu",
+  props:{
+    name:{
+      type: String,
+      required: true
+    },
+    data :{
+      type: Array,
+      required: true
+    },
+    columns: {
+      type: Array,
+      required: true
+    },
+    bordered:{
+      type: String,
+      required: true
+    },
+    paginated: {
+      type: Boolean,
+      required: true
+    },
+    perPage:{
+      type: Number,
+      required: true
+    }
+
+  },
+  components: {},
+  mixins: [utils],
+  mounted(){
+    console.log(this.name,' is mounted');
+    this.$emit('cevt', ['setCmdHandler', this.handleCmd, this.name]);
+    for(var c=0;c<this.columns.length;c++){
+      var columnIsNumeric = false;
+      if(typeof(this.columns[c].numeric)!='undefined'){
+        columnIsNumeric=this.columns[c].numeric
+      }
+      var columnStyle = "width:"+this.columns[c].width+";";
+      var columnVisible=true;
+      if(typeof(this.columns[c].visible)!='undefined'){
+        columnVisible=this.columns[c].visible;
+      }
+      var thisStyledColumn = {
+        label: this.columns[c].label,
+        field: this.columns[c].field,
+        numeric: columnIsNumeric,
+        columnStyle:columnStyle,
+        visible:columnVisible
+      }
+      this.styledColumns.push(thisStyledColumn);
+    }
+
+  },
+  beforeDestroy() {
+    this.$emit('cevt', ['removeCmdHandler', this.handleCmd, this.name]);
+  },
+  data(){
+    return {
+      cmdHandlers:{},
+      leafComponent: false,
+      styledColumns:[],
+    }
+  },
+  methods:{
+//cmd handlers
+    handleCmd(args){
+      console.log(this.name, ' handleCmd', args);
+      this.cmdHandler(args, this);
+    },
+    cmdHandler(args, self){
+      if(args[2]==this.name || this.leafComponent==false){
+        var cmdType ={
+          'default': function(context, args){
+            console.log('cmdHandler in dummy - something else', args, context);
+          }
+          /*  example
+                    'setCardMode':function(args, context){
+                      self.doSetCardMode(args, context);
+                    }
+          */
+        }
+        if(typeof(cmdType)!='undefined'){
+          try {
+            (cmdType[args[0]](args, self));
+          } catch (e) {
+            console.log('unknown cmd -',args, this.name);
+            console.log('unknown cmd -',args, this.name);
+            var availableHandlers = Object.keys(this.cmdHandlers);
+            console.log('available cmd handlers-',availableHandlers);
+
+            if(availableHandlers.length>0){
+              for(var a=0;a<availableHandlers.length;a++){
+                //                       debugger;
+                this.cmdHandlers[availableHandlers[a]]([args[0], args[1], args[2]]);
+              }
+            }
+          }
+        }
+      }
+    },
+// put do cmds here
+
+//event handler
+    evtOpt(msg){
+      console.log('evtOpt in menu', msg);
+      this.evtHandler(msg, this);
+    },
+    evtHandler(msg, self){
+      console.log('evtHandler in- (set this)', msg, self);
+//      debugger;
+      var evtType = {
+        'setCmdHandler': function(msg, context){
+          //console.log('evtHandler - a menu event', msg);
+          context.doSetCmdHandler(msg, context);
+        },
+        'removeCmdHandler': function(msg, context){
+          context.doRemoveCmdHandler(msg, context);
+        },
+        'default': function(msg, context){
+          console.log('evtHandler in menu  - something else', msg, context);
+        }
+      }
+      if(typeof(evtType)!='undefined'){
+        try {
+          (evtType[msg[0]](msg, self));
+        } catch (e) {
+          this.$emit('cevt', msg);
+        }
+      }
+    },
+    doSetCmdHandler(msg, context){
+//      debugger;
+      console.log('doSetCmdHandler-',msg, context);
+      this.cmdHandlers[msg[2]]=msg[1];
+    },
+    doRemoveCmdHandler(msg, context){
+      console.log('doRemoveCmdHandler-',msg, context);
+      delete(this.cmdHandlers[msg[2]]);
+    },
+    doMenuItemSelected(msg, context){
+      console.log('headlineCard doMenuItemSelected-', msg, context);
+      debugger;
+      if(msg.length>3){
+        msg[3](this);
+      }else{
+        switch(msg[2]){
+          default:{
+            this.$emit('cevt', msg);
+          }
+        }
+      }
+    },
+
+
+  }
+}
+</script>
+
+<style scoped>
+.styled-table {
+  border-collapse: collapse;
+  margin: 25px 0;
+  font-size: 0.9em;
+  font-family: sans-serif;
+  min-width: 400px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
+.styled-table thead tr {
+  background-color: #009879;
+  color: #ffffff;
+  text-align: left;
+}
+.styled-table th,
+.styled-table td {
+  padding: 12px 15px;
+}
+.styled-table tbody tr {
+  border-bottom: 1px solid #dddddd;
+}
+
+.styled-table tbody tr:nth-of-type(even) {
+  background-color: #f3f3f3;
+}
+
+.styled-table tbody tr:last-of-type {
+  border-bottom: 2px solid #009879;
+}
+.styled-table tbody tr.active-row {
+  font-weight: bold;
+  color: #009879;
+}
+
+</style>
